af2b57f8540fe30459b8384c2aebeec577b0da21
Emma Bjørg <emso0005@stud.kea.dk>
2024-03-21 15:37:38 +0100

    header-menu

--- public/main.js
+++ public/main.js
@@ -317,3 +317,16 @@ function initMap() {
     createInfoWindows ();
 }
     
+//__________________//
+
+//menu
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navigation = document.getElementById("navigation");
+  
+    menuToggle.addEventListener("click", function() {
+      navigation.classList.toggle("active");
+      menuToggle.classList.toggle("darken");
+    });
+  });
